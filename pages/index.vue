8227b0f8fe5f6b49a795539185d2099003b88fae
Dani Edo <dani.nur.afandi@gmail.com>
2020-11-15 22:54:42 +0700

    hero image

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
 <template>
   <div>
-    <h1 class="hero"><b>Init SCSS</b></h1>
+    <div :class="['hero-image', { scrolled: scrolled }]">
+      <img src="/img/bali.jpg" alt="bali" />
+      <div class="caption">
+        <h1>My Adventure</h1>
+        <p>Make your dream come true</p>
+      </div>
+    </div>
+    <div class="content">hello</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Home',
+  data() {
+    return {
+      scrolled: false,
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      if (window.scrollY === 0) {
+        this.scrolled = false
+      } else {
+        this.scrolled = true
+      }
+    },
+  },
+}
 </script>
