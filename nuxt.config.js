8b32a6bf7e12e4d21e42e9061d7d3a985c080bd4
Dani Edo <dani.nur.afandi@gmail.com>
2020-11-15 22:10:14 +0700

    sass config

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    // SCSS file in the project
+    '@/assets/style/style.scss',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
